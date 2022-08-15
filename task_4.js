ecb4f676c7c41b419443c92226fc2bd8cf9301dc
KonstantinChukavin <KainkosYa@yandex.ru>
2022-08-15 14:33:33 +0300

    complited task 4 & 5

--- task_4.js
+++ task_4.js
@@ -4,13 +4,32 @@
 Определите иерархию электроприборов. Включите некоторые в розетку. Посчитайте потребляемую мощность (передайте аргумент). 
 
 Таких приборов должно быть как минимум два (например, настольная лампа и компьютер). Выбрав прибор, подумайте, какими свойствами он обладает.
-
-План:
-
-Определите родительскую функцию с методами, которые включают/выключают прибор из розетки.
-Создайте делегирующую связь [[Prototype]] для двух конкретных приборов.
-У каждого из приборов должны быть собственные свойства и, желательно, методы, отличные от родительских методов.
-Создайте экземпляры каждого прибора.
-Выведите в консоль и посмотрите на результаты работы, можете гордиться собой :)
 */
 
+function Device(smartphone, power) {
+    this.score = 'citylink',
+        this.capacity = function () {
+            console.log(`Battery capacity ${smartphone} is - ${power} mAh`);
+        }
+}
+
+const samsung = new Device('galaxy S22 Ultra', 5000);
+samsung.system = 'Android 12';
+samsung.cpu = 'Samsung Exynos 2200';
+samsung.price = '2020$';
+
+const apple = new Device('iPhone 13 Pro Max', 4352);
+apple.system = 'iPhone iOS 15';
+apple.cpu = 'Apple A15 Bionic';
+apple.price = '2250$';
+
+const xiaomi = new Device('Poco M3 Pro 6', 5000);
+xiaomi.gpu = 'ARM Mali-G57';
+xiaomi.cpu = 'MediaTek Dimensity 700';
+xiaomi.price = '260$';
+
+samsung.capacity();
+apple.capacity();
+xiaomi.capacity();
+
+console.log('apple', apple, 'samsung', samsung, 'xiaomi', xiaomi);
---
+++ task_5.js
@@ -0,0 +1,33 @@
+/*
+Переписать консольное приложение из предыдущего юнита на классы.
+*/
+
+class Device {
+    constructor(smartphone, power) {
+        this.score = 'citylink',
+            this.capacity = function () {
+                console.log(`Battery capacity ${smartphone} is - ${power} mAh`);
+            };
+    }
+}
+
+const samsung = new Device('galaxy S22 Ultra', 5000);
+samsung.system = 'Android 12';
+samsung.cpu = 'Samsung Exynos 2200';
+samsung.price = '2020$';
+
+const apple = new Device('iPhone 13 Pro Max', 4352);
+apple.system = 'iPhone iOS 15';
+apple.cpu = 'Apple A15 Bionic';
+apple.price = '2250$';
+
+const xiaomi = new Device('Poco M3 Pro 6', 5000);
+xiaomi.gpu = 'ARM Mali-G57';
+xiaomi.cpu = 'MediaTek Dimensity 700';
+xiaomi.price = '260$';
+
+samsung.capacity();
+apple.capacity();
+xiaomi.capacity();
+
+console.log('apple', apple, 'samsung', samsung, 'xiaomi', xiaomi);
